7befdf8d5d0b0951e8fd1d1628c5f7712ac580fc
Ramazan <romagt2015@mail.ru>
2024-12-28 11:40:30 +0300

    Version 2

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,28 @@
-<script></script>
+<script>
+import { defineComponent, ref } from 'vue'
+import home from './views/home.vue'
+import Test from './views/Test.vue'
+
+export default defineComponent({
+  components: {
+    home,
+    Test,
+  },
+  setup() {
+    const blockIndex = ref(0)
+
+    const startQuiz = (index) => {
+      blockIndex.value = index
+    }
+    return {
+      blockIndex,
+      startQuiz,
+    }
+  },
+})
+</script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink v-if="route$.path === '/'" to="/">Home</RouterLink>
-        <RouterLink v-else to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <home v-if="$route.path === '/'" @start-quiz="startQuiz" />
+  <Test v-else :blockIndex="blockIndex" />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
